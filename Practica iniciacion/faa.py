ab7b9ef1e285f9306ccb0519a7ff2f7e20836f92
Yyandrakk <oscargdlp@gmail.com>
2017-09-18 10:23:35 +0200

    datos de moodle

---
+++ Practica iniciacion/faa.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+class Datos(object):
+  
+  TiposDeAtributos=('Continuo','Nominal')
+  tipoAtributos=[]
+  nombreAtributos=[]
+  nominalAtributos=[]
+  datos=np.array(())
+  # Lista de diccionarios. Uno por cada atributo.
+  diccionarios=[]
+ 
+  # TODO: procesar el fichero para asignar correctamente las variables tipoAtributos, nombreAtributos,nominalAtributos, datos y diccionarios
+  def __init__(self, nombreFichero):
+    
+  # TODO: implementar en la práctica 1
+  def extraeDatosTrain(idx):
+    pass
+  
+  def extraeDatosTest(idx):
+    pass
+
+
+
+  
