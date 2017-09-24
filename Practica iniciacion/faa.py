5960c98158b313ede3a38797fd5b5f6f8b51d8d4
Yyandrakk <oscargdlp@gmail.com>
2017-09-24 18:42:18 +0200

    abriendo el fichero

--- Practica iniciacion/faa.py
+++ Practica iniciacion/faa.py
@@ -1,25 +1,27 @@
 import numpy as np
 
+
 class Datos(object):
-  
-  TiposDeAtributos=('Continuo','Nominal')
-  tipoAtributos=[]
-  nombreAtributos=[]
-  nominalAtributos=[]
-  datos=np.array(())
-  # Lista de diccionarios. Uno por cada atributo.
-  diccionarios=[]
- 
-  # TODO: procesar el fichero para asignar correctamente las variables tipoAtributos, nombreAtributos,nominalAtributos, datos y diccionarios
-  def __init__(self, nombreFichero):
-    
-  # TODO: implementar en la práctica 1
-  def extraeDatosTrain(idx):
-    pass
-  
-  def extraeDatosTest(idx):
-    pass
+
+    TiposDeAtributos = ('Continuo', 'Nominal')
+    tipoAtributos = []
+    nombreAtributos = []
+    nominalAtributos = []
+    datos = np.array(())
+    # Lista de diccionarios. Uno por cada atributo.
+    diccionarios = []
+    # TODO: procesar el fichero para asignar correctamente las variables
+    #  tipoAtributos, nombreAtributos,nominalAtributos, datos y diccionarios
+
+    def __init__(self, nombreFichero):
+        f = open(nombreFichero, 'r')
+        
 
 
+    # TODO: implementar en la prï¿½ctica 1
 
-  
+    def extraeDatosTrain(idx):
+        pass
+
+    def extraeDatosTest(idx):
+        pass
