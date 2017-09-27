577040011cd2a95fba6fe57ef17721cf6e10760f
Yyandrakk <oscargdlp@gmail.com>
2017-09-27 19:26:56 +0200

    terminado

--- Practica iniciacion/faa.py
+++ Practica iniciacion/faa.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import sys
 
 class Datos(object):
 
@@ -15,15 +15,59 @@ class Datos(object):
 
     def __init__(self, nombreFichero):
         with open(nombreFichero, 'r') as f:
-            nDatos =    int(f.readline().strip('\n'))
-            
+            linesL=f.read().splitlines()
+            con,nom = self.TiposDeAtributos
+            self.nDatos = int(linesL[0])
+            self.nombreAtributos = linesL[1].split(',')
+            self.tipoAtributos = linesL[2].split(',')
+            self.nominalAtributos = map(lambda x: True if x == nom else False, self.tipoAtributos)
+            datosAux = np.array(map(lambda x:list(x.split(',')),linesL[3:]))
+            i=0
+            tam=len(self.nominalAtributos)
+            while i<tam:
+                if not self.nominalAtributos[i]:
+                    self.diccionarios.append({})
+                else:
+                    auxC = set(datosAux[:,i])
+                    j=0
+                    dicAux={}
+                    for x in sorted(auxC):
+                        dicAux[x]=j
+                        j+=1
+                    self.diccionarios.append(dicAux)
+                i+=1
+            i=0
+            self.datos=np.empty((self.nDatos,tam))
+            while i<tam:
+                if not self.nominalAtributos[i]:
+                    self.datos[:,i]=datosAux[:,i]
+                else:
+                    dic=self.diccionarios[i]
+                    j=0
+                    while j<self.nDatos:
+                        self.datos[j,i]=dic[datosAux[j,i]]
+                        j+=1
+                i+=1
+
+
+            print self.nDatos
+            print self.nombreAtributos
+            print self.tipoAtributos
+            print self.nominalAtributos
+            print self.diccionarios
+            print self.datos
 
 
 
-    # TODO: implementar en la pr�ctica 1
+
+
+    # TODO: implementar en la prctica 1
 
     def extraeDatosTrain(idx):
         pass
 
     def extraeDatosTest(idx):
         pass
+
+if __name__ == '__main__':
+    datos= Datos(sys.argv[1])
