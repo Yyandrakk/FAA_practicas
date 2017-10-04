23361fe315e7978abb469997e374fb4063a31eef
Yyandrakk <oscargdlp@gmail.com>
2017-10-04 19:36:19 +0200

    Semana 1 hecha, sin comprobar

--- Practica 1/Datos.py
+++ Practica 1/Datos.py
@@ -1,54 +1,56 @@
 import numpy as np
 
+
 class Datos(object):
 
-  TiposDeAtributos=('Continuo','Nominal')
-  tipoAtributos=[]
-  nombreAtributos=[]
-  nominalAtributos=[]
-  datos=np.array(())
-  # Lista de diccionarios. Uno por cada atributo.
-  diccionarios=[]
+    TiposDeAtributos = ('Continuo', 'Nominal')
+    tipoAtributos = []
+    nombreAtributos = []
+    nominalAtributos = []
+    datos = np.array(())
+    # Lista de diccionarios. Uno por cada atributo.
+    diccionarios = []
 
-  # TODO: procesar el fichero para asignar correctamente las variables tipoAtributos, nombreAtributos,nominalAtributos, datos y diccionarios
-  def __init__(self, nombreFichero):
+    # TODO: procesar el fichero para asignar correctamente las variables tipoAtributos, nombreAtributos,nominalAtributos, datos y diccionarios
+    def __init__(self, nombreFichero):
         with open(nombreFichero, 'r') as f:
-            linesL=f.read().splitlines()
-            con,nom = self.TiposDeAtributos
+            linesL = f.read().splitlines()
+            con, nom = self.TiposDeAtributos
             self.nDatos = int(linesL[0])
             self.nombreAtributos = linesL[1].split(',')
             self.tipoAtributos = linesL[2].split(',')
             self.nominalAtributos = map(lambda x: True if x == nom else False, self.tipoAtributos)
-            datosAux = np.array(map(lambda x:list(x.split(',')),linesL[3:]))
-            i=0
-            tam=len(self.nominalAtributos)
-            while i<tam:
+            datosAux = np.array(map(lambda x: list(x.split(',')), linesL[3:]))
+            i = 0
+            tam = len(self.nominalAtributos)
+            while i < tam:
                 if not self.nominalAtributos[i]:
                     self.diccionarios.append({})
                 else:
-                    auxC = set(datosAux[:,i])
-                    j=0
-                    dicAux={}
+                    auxC = set(datosAux[:, i])
+                    j = 0
+                    dicAux = {}
                     for x in sorted(auxC):
-                        dicAux[x]=j
-                        j+=1
-                    self.diccionarios.append(dicAux)
-                i+=1
-            i=0
-            self.datos=np.empty((self.nDatos,tam))
-            while i<tam:
-                if not self.nominalAtributos[i]:
-                    self.datos[:,i]=datosAux[:,i]
-                else:
-                    dic=self.diccionarios[i]
-                    j=0
-                    while j<self.nDatos:
-                        self.datos[j,i]=dic[datosAux[j,i]]
-                        j+=1
-                i+=1
-  # TODO: implementar en la pr�ctica 1
-  def extraeDatosTrain(idx):
-    pass
+                        dicAux[x] = j
+                        j += 1
+                        self.diccionarios.append(dicAux)
+                        i += 1
+                        i = 0
+                        self.datos = np.empty((self.nDatos, tam))
+                        while i < tam:
+                            if not self.nominalAtributos[i]:
+                                self.datos[:, i] = datosAux[:, i]
+                            else:
+                                dic = self.diccionarios[i]
+                                j = 0
+                                while j < self.nDatos:
+                                    self.datos[j, i] = dic[datosAux[j, i]]
+                                    j += 1
+                                    i += 1
 
-  def extraeDatosTest(idx):
-    pass
+# TODO: implementar en la pr�ctica 1
+    def extraeDatosTrain(self, idx):
+        return self.datos[idx, :]
+
+    def extraeDatosTest(self, idx):
+        return self.datos[idx, :]
--- Practica 1/EstrategiaParticionado.py
+++ Practica 1/EstrategiaParticionado.py
@@ -1,62 +1,85 @@
-from abc import ABCMeta,abstractmethod
+from abc import ABCMeta, abstractmethod
+import random
+import numpy as np
+import math
 
 
 class Particion():
-  
-  indicesTrain=[]
-  indicesTest=[]
-  
-  def __init__(self):
-    self.indicesTrain=[]
-    self.indicesTest=[]
+
+    indicesTrain = []
+    indicesTest = []
+
+    def __init__(self):
+        self.indicesTrain = []
+        self.indicesTest = []
 
 #####################################################################################################
 
+
 class EstrategiaParticionado(object):
-  
-  # Clase abstracta
-  __metaclass__ = ABCMeta
-  
-  # Atributos: deben rellenarse adecuadamente para cada estrategia concreta
-  nombreEstrategia="null"
-  numeroParticiones=0
-  particiones=[]
-  
-  
-  
-  @abstractmethod
-  # TODO: esta funcion deben ser implementadas en cada estrategia concreta  
-  def creaParticiones(self,datos,seed=None):
-    pass
-  
+
+    # Clase abstracta
+    __metaclass__ = ABCMeta
+
+    # Atributos: deben rellenarse adecuadamente para cada estrategia concreta
+    nombreEstrategia = "null"
+    numeroParticiones = 0
+    particiones = []
+
+    @abstractmethod
+    # TODO: esta funcion deben ser implementadas en cada estrategia concreta
+    def creaParticiones(self, datos, seed=None):
+        pass
+
 
 #####################################################################################################
 
 class ValidacionSimple(EstrategiaParticionado):
-  
-  
-  # Crea particiones segun el metodo tradicional de division de los datos segun el porcentaje deseado.
-  # Devuelve una lista de particiones (clase Particion)
-  # TODO: implementar
-  def creaParticiones(self,datos,seed=None):
-    
-    random.seed(seed)
-    pass
-      
-      
-#####################################################################################################      
-class ValidacionCruzada(EstrategiaParticionado):
-  
-  
 
-  
-  # Crea particiones segun el metodo de validacion cruzada.
-  # El conjunto de entrenamiento se crea con las nfolds-1 particiones
-  # y el de test con la particion restante
-  # Esta funcion devuelve una lista de particiones (clase Particion)
-  # TODO: implementar
-  def creaParticiones(self,datos,seed=None):   
-    random.seed(seed)
-    pass
-    
-    
+    def __init__(self, k):
+        self.nombreEstrategia = "ValidacionSimple"
+        self.numeroParticiones = k
+
+    # Crea particiones segun el metodo tradicional de division de los datos segun el porcentaje deseado.
+    # Devuelve una lista de particiones (clase Particion)
+    # TODO: implementar
+    def creaParticiones(self, datos, seed=None):
+        random.seed(seed)
+        numFilas = datos.Rows.Count
+        particion = Particion()
+        index = xrange(0, numFilas)
+        random.shuffle(index)
+        numTrain = math.ceil(numFilas * self.numeroParticiones)
+        particion.indicesTrain = index[0: numTrain]
+        particion.indicesTest = index[numTrain + 1:]
+        self.particiones.append(particion)
+
+#####################################################################################################
+
+
+class ValidacionCruzada(EstrategiaParticionado):
+
+    def __init__(self, k):
+        self.nombreEstrategia = "ValidacionCruzada"
+        self.numeroParticiones = k
+    # Crea particiones segun el metodo de validacion cruzada.
+    # El conjunto de entrenamiento se crea con las nfolds-1 particiones
+    # y el de test con la particion restante
+    # Esta funcion devuelve una lista de particiones (clase Particion)
+    # TODO: implementar
+
+    def creaParticiones(self, datos, seed=None):
+        random.seed(seed)
+        numFilas = datos.Rows.Count
+        index = xrange(0, numFilas)
+        random.shuffle(index)
+        indexK = numFilas // self.numeroParticiones
+        lIndex = [index[i:i + indexK] for i in xrange(0, len(index), indexK)]
+        i = 0
+        while i < self.numeroParticiones:
+            particion = Particion()
+            particion.indicesTest = lIndex[i]
+            auxL = [x for j, x in enumerate(lIndex) if j != i]
+            particion.indicesTrain = [x for subL in auxL for x in subL]
+            self.particiones.append(particion)
+            i += 1
