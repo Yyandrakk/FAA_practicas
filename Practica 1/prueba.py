6c57eb6f1db7c75f090e36055c2580b26a1bf3f2
ricardomoratomateos <ricardomoratomateos@outlook.com>
2017-10-14 08:21:47 -0700

    Anyadido NaiveBayes de sklearn

--- Practica 1/prueba.py
+++ Practica 1/prueba.py
@@ -1,6 +1,9 @@
 import Clasificador
 import EstrategiaParticionado
 from Datos import Datos
+from sklearn import preprocessing
+from sklearn.model_selection import cross_val_score
+from sklearn.naive_bayes import GaussianNB
 
 if __name__ == '__main__':
     dataset = Datos('./ConjuntosDatos/german.data')
@@ -8,3 +11,13 @@ if __name__ == '__main__':
     clasificador = Clasificador.ClasificadorNaiveBayes()
     errores = clasificador.validacion(estrategia, dataset, clasificador)
     print errores
+
+
+    # Encode categorical integer features using a one-hot aka one-of-K scheme(categorical features)
+    encAtributos = preprocessing.OneHotEncoder(categorical_features=dataset.nominalAtributos[:-1],sparse=False)
+    X = encAtributos.fit_transform(dataset.datos[:,:-1])
+    Y = dataset.datos[:,-1]
+    clf = GaussianNB()
+    clf.fit(X, Y)
+    GaussianNB(priors=None)
+    print cross_val_score(clf, X, Y)
