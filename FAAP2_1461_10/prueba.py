148a41b16c6e8b9ea4759408660c62429984fa98
Oscar <oscargdlp@gmail.com>
2017-11-11 20:47:33 +0100

    notebook example 1 RL

--- FAAP2_1461_10/prueba.py
+++ FAAP2_1461_10/prueba.py
@@ -12,25 +12,29 @@ from sklearn.model_selection import train_test_split
 
 if __name__ == '__main__':
     errores =     []
-    dataset = Datos('./ConjuntoDatos/example3.data')
-    estrategia = EstrategiaParticionado.ValidacionSimple()
-    clasificador = Clasificador.ClasificadorRegresionLogistica(nEpoc=100,consApren=0.05)
+    dataset = Datos('./ConjuntoDatos/example1.data')
+    estrategia = EstrategiaParticionado.ValidacionCruzada()
+    #clasificador = Clasificador.ClasificadorRegresionLogistica(nEpoc=100,consApren=0.05)
    # error_media, error_std = clasificador.validacion(estrategia, dataset, clasificador,45)
     #print error_media
 
-    encAtributos = preprocessing.OneHotEncoder(categorical_features=dataset.nominalAtributos[:-1], sparse=False)
-    X = encAtributos.fit_transform(dataset.datos[:, :-1])
-    Y = dataset.datos[:, -1]
+    clasificador = Clasificador.ClasificadorRegresionLogistica(nEpoc=1000, consApren=0.01)
+    error_media, error_std = clasificador.validacion(estrategia, dataset, clasificador, 45)
+    print error_media
 
-    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.4, random_state=42)
-
-    # instantiate a logistic regression model, and fit with X and y
-    model = LogisticRegression(max_iter=10,class_weight=None)
-    pre=model.fit(x_train, y_train).predict(x_test)
-
-    # check the accuracy on the training set
-    print 1 - accuracy_score(y_test, pre)
-    estrategia.creaParticiones(dataset.datos)
+    # encAtributos = preprocessing.OneHotEncoder(categorical_features=dataset.nominalAtributos[:-1], sparse=False)
+    # X = encAtributos.fit_transform(dataset.datos[:, :-1])
+    # Y = dataset.datos[:, -1]
+    #
+    # x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.4, random_state=42)
+    #
+    # # instantiate a logistic regression model, and fit with X and y
+    # model = LogisticRegression(max_iter=10,class_weight=None)
+    # pre=model.fit(x_train, y_train).predict(x_test)
+    #
+    # # check the accuracy on the training set
+    # print 1 - accuracy_score(y_test, pre)
+    # estrategia.creaParticiones(dataset.datos)
 
     ii = estrategia.particiones[-1].indicesTrain
     clasificador.entrenamiento(dataset.extraeDatosTrain(ii),dataset.nominalAtributos,dataset.diccionarios)
