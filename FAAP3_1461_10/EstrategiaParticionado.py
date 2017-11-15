cca609398502b0164feaed71554a75be68ba5c36
Yyandrakk <oscargdlp@gmail.com>
2017-11-15 19:39:45 +0100

    inicio p3

---
+++ FAAP3_1461_10/EstrategiaParticionado.py
@@ -0,0 +1,82 @@
+import math
+import random
+from abc import ABCMeta, abstractmethod
+
+
+class Particion():
+
+    def __init__(self):
+        self.indicesTrain = []
+        self.indicesTest = []
+
+#####################################################################################################
+
+
+class EstrategiaParticionado(object):
+
+    # Clase abstracta
+    __metaclass__ = ABCMeta
+
+    # Atributos: deben rellenarse adecuadamente para cada estrategia concreta
+
+
+    @abstractmethod
+    # TODO: esta funcion deben ser implementadas en cada estrategia concreta
+    def creaParticiones(self, datos, seed=None):
+        pass
+
+
+#####################################################################################################
+
+class ValidacionSimple(EstrategiaParticionado):
+
+    def __init__(self, porcentajeTrain=0.6):
+        self.nombreEstrategia = "ValidacionSimple"
+        self.numeroParticiones = porcentajeTrain
+        self.particiones = []
+        
+
+    # Crea particiones segun el metodo tradicional de division de los datos segun el porcentaje deseado.
+    # Devuelve una lista de particiones (clase Particion)
+    # TODO: implementar
+    def creaParticiones(self, datos, seed=None):
+        random.seed(seed)
+        numFilas = datos.shape[0]
+        particion = Particion()
+        index = list(xrange(0, numFilas))
+        random.shuffle(index)
+        numTrain = int(math.ceil(numFilas * self.numeroParticiones))
+        particion.indicesTrain = index[0: numTrain]
+        particion.indicesTest = index[numTrain + 1:]
+        self.particiones.append(particion)
+
+#####################################################################################################
+
+
+class ValidacionCruzada(EstrategiaParticionado):
+
+    def __init__(self, k=10):
+        self.nombreEstrategia = "ValidacionCruzada"
+        self.numeroParticiones = k
+        self.particiones = []
+    # Crea particiones segun el metodo de validacion cruzada.
+    # El conjunto de entrenamiento se crea con las nfolds-1 particiones
+    # y el de test con la particion restante
+    # Esta funcion devuelve una lista de particiones (clase Particion)
+    # TODO: implementar
+
+    def creaParticiones(self, datos, seed=None):
+        random.seed(seed)
+        numFilas = datos.shape[0]
+        index = list(xrange(0, numFilas))
+        random.shuffle(index)
+        indexK = numFilas // self.numeroParticiones
+        lIndex = [index[i:i + indexK] for i in xrange(0, len(index), indexK)]
+        i = 0
+        while i < self.numeroParticiones:
+            particion = Particion()
+            particion.indicesTest = lIndex[i]
+            auxL = [x for j, x in enumerate(lIndex) if j != i]
+            particion.indicesTrain = [x for subL in auxL for x in subL]
+            self.particiones.append(particion)
+            i += 1
---
+++ FAAP3_1461_10/PreprocesamientoAG.py
@@ -0,0 +1,39 @@
+import Clasificador
+import EstrategiaParticionado
+from Datos import Datos
+import numpy as np
+
+
+class PreprocesamientoAG(object):
+
+    def __init__(self,pCruce=0.6,pMut=0.001,pElitismo=0.05, tamPob=50, gener=(50,0.95)):
+        self.pCruce = pCruce
+        self.pMut=pMut
+        self.pElitismo=pElitismo
+        self.tamPob=tamPob
+        self.geraciones=gener
+
+
+    def seleccionarAtributos(self,dataset,clasificador):
+        '''
+
+
+        :param dataset:
+        :type dataset: Datos
+        :param clasificador:
+        :type clasificador: Clasificador
+        :return:
+        '''
+        if type(dataset) != Datos or type(clasificador) != Clasificador:
+            raise TypeError, "dataset debe ser tipo Datos y clasificador de tipo Clasificador"
+
+        generacionMax,pParada = self.geraciones
+        poblacion =np.unpackbits(np.random.randint(low=1,high=len(dataset.diccionarios), size=(self.tamPob,1),dtype=np.uint8),axis=1)
+
+        g=0
+        while g<generacionMax and p<pParada:
+
+
+
+
+            g+=1
