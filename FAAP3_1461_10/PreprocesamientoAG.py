eaf0c77c3a7a14932c3eaf6c4700396905018c33
ricardomoratomateos <ricardomoratomateos@outlook.com>
2017-11-17 19:18:46 +0100

    Cambios
    Prueba

--- FAAP3_1461_10/PreprocesamientoAG.py
+++ FAAP3_1461_10/PreprocesamientoAG.py
@@ -1,6 +1,10 @@
 import Clasificador
 import EstrategiaParticionado
 from Datos import Datos
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
 import numpy as np
 
 
@@ -13,11 +17,8 @@ class PreprocesamientoAG(object):
         self.tamPob=tamPob
         self.geraciones=gener
 
-
     def seleccionarAtributos(self,dataset,clasificador):
         '''
-
-
         :param dataset:
         :type dataset: Datos
         :param clasificador:
@@ -27,13 +28,13 @@ class PreprocesamientoAG(object):
         if type(dataset) != Datos or type(clasificador) != Clasificador:
             raise TypeError, "dataset debe ser tipo Datos y clasificador de tipo Clasificador"
 
-        generacionMax,pParada = self.geraciones
-        poblacion =np.unpackbits(np.random.randint(low=1,high=len(dataset.diccionarios), size=(self.tamPob,1),dtype=np.uint8),axis=1)
-
-        g=0
-        while g<generacionMax and p<pParada:
-
-
-
-
+        generacionMax, pParada = self.geraciones
+        poblacion = np.unpackbits(np.random.randint(low=1,high=len(dataset.diccionarios), size=(self.tamPob,1),dtype=np.uint8),axis=1)
+        dat = dataset.extraeDatosRelevantes(poblacion)
+        g = 0
+        p = 1
+        estrategia = EstrategiaParticionado.ValidacionSimple()
+        while g < generacionMax and p < pParada:
+            error_media, error_std = clasificador.validacion(estrategia, dat, clasificador, 42)
+            p = 1 - error_media
             g+=1
